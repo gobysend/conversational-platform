31076449764fe13fadbe1481d89c8f29cb3a879f
Dat Nguyen <dat.nguyen293@gmail.com>
2021-08-25 14:43:54 +0700

    Whitelisted all IPs on development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Allow webhooks from any IP
+  config.web_console.whiny_requests = false
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?
