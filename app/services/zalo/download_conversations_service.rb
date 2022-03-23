ffb5df3947236dfeb7621d4e412adf8da6de7f4f
Dat Nguyen <dat.nguyen293@gmail.com>
2022-03-23 11:10:47 +0700

    Fixed issue of sync Zalo conversation

--- app/services/zalo/download_conversations_service.rb
+++ app/services/zalo/download_conversations_service.rb
@@ -116,10 +116,12 @@ class Zalo::DownloadConversationsService
   def contact_inbox(thread)
     @sender_id = thread[:src] == 1 ? thread[:from_id] : thread[:to_id]
 
-    @contact ||= @inbox.contact_inboxes.find_by(source_id: @sender_id)&.contact
-    @contact ||= Contact.create!(contact_params(thread).except(:remote_avatar_url))
-
     @contact_inbox = @inbox.contact_inboxes.find_by(source_id: @sender_id)
+    @contact ||= @contact_inbox&.contact
+
+    return if @contact.present?
+
+    @contact = Contact.create!(contact_params(thread).except(:remote_avatar_url))
     @contact_inbox ||= ContactInbox.create(contact: @contact, inbox: @inbox, source_id: @sender_id)
   end
 
